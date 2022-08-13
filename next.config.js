c2f31fe443b2c5998d80c471029007b0c6de759f
Obaid Qatan <ogaten27@gmail.com>
2022-08-14 02:34:49 +0530

    build

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
+const nextTranslate = require('next-translate')
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = nextTranslate({
   reactStrictMode: true,
   swcMinify: true,
-}
+
+  i18n: {
+    locales: ['en', 'ar'],
+    defaultLocale: 'en',
+  },
+})
 
 module.exports = nextConfig
