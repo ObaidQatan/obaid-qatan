72104f9fd04e1ab0ab2c011d1c1f669d2b187ec4
Obaid Qatan <ogaten27@gmail.com>
2022-09-13 01:18:32 +0530

    add meta

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -1,8 +1,19 @@
 const SEO = () => (
-  <meta
-    name="description"
-    content="My name is Obaid Qatan and this is my profile."
-  />
+  <>
+    <meta name="title" content="Obaid Qatan" />
+    <meta
+      name="description"
+      content="My name is Obaid Qatan and this is my web page."
+    />
+    <meta
+      name="keywords"
+      content="obaid,qatan,sadeq,obaid qatan,obaid-qatan,obaidqatan,obaid sadeq qatan,obaidsadeq,obaid-sadeq,obaidsadeqqatan,obaid-sadeq-qatan,developer,website,programmer,engineer,computer,computer science,portfolio"
+    />
+    <meta name="robots" content="index, follow" />
+    <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="language" content="English" />
+    <meta name="author" content="Obaid Qatan" />
+  </>
 );
 
 export default SEO;
