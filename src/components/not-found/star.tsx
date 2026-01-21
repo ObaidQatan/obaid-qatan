810c45fe4477464cd34543042fb6ebebcd88749e
Obaid Qatan <ogaten27@gmail.com>
2026-01-21 20:49:55 +0700

    fix not-found

--- src/components/not-found/star.tsx
+++ src/components/not-found/star.tsx
@@ -1,13 +1,23 @@
 "use client";
+
 import { random } from "lodash";
 import { useMemo, useState } from "react";
 
+const getRandomPosition = () => {
+  if (typeof window === "undefined") return { x: 0, y: 0 };
+
+  return {
+    x: random(true) * window.innerWidth,
+    y: random(true) * window.innerHeight,
+  };
+};
+
 const Star = () => {
-  const [x] = useState(random(true) * window.innerWidth);
-  const [y] = useState(random(true) * window.innerHeight);
-  const [size] = useState(random(true) * 2);
+  const [{ x, y }] = useState(getRandomPosition);
+
+  const [size] = useState(() => random(true) * 2);
   const [color] = useState(
-    `#${Math.floor(random(true) * 16777215).toString(16)}`,
+    () => `#${Math.floor(random(true) * 16777215).toString(16)}`,
   );
 
   return useMemo(
